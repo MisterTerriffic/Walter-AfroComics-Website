a391a08985a12e9a8de8e28a9527e8f79bba7efa
Walter Fisher <o.b.i.e@Mac.lan>
2025-04-29 16:09:08 -0500

    Added jsx files and js files for data.

---
+++ data/comics.js
@@ -0,0 +1,32 @@
+const comics = [
+  {
+    id: "",
+    title: "Blood Syndicate: The Origin",
+    author: "Ivan Velez Jr",
+    artist: "Denys Cowan & Jimmy Palmiotti",
+    publisher: "DC Comics",
+    published: "1993",
+    about: "",
+    imageUrl: "./images/Bloodsyndicatemsu0.jpeg",
+  },
+  {
+    id: "",
+    title: "Bitter Root #1",
+    author: "Chuck Brown",
+    artist: "Sanford Green",
+    publisher: "Image Comics",
+    published: "2018",
+    about: "",
+    imageUrl: "./Image/Black_Vol1_800x.jpg",
+  },
+  {
+    id: "",
+    title: "Black Vol 1",
+    author: "Kwanza Osajyefo",
+    artist: "Robin Riggs",
+    publisher: " Black Mask Comics",
+    published: "2017",
+    about: "",
+    imageUrl: "./Image/IMG_0348.jpeg",
+  },
+];
