b699e551037c097c8f770a24f9c3942ec32e2ee6
Walter Fisher <o.b.i.e@OBIE-2.local>
2025-06-18 19:55:56 -0500

    added data to the js files.

--- src/data/funkopops.js
+++ src/data/funkopops.js
@@ -1,11 +1,11 @@
 const funkoPops = [
     {
-        itemNumber: "",
+        itemNumber: "001",
         imageUrl: "",
-        name: "",
-        license: "",
-        details: "",
-        price: 1,
+        name: "Black Panther",
+        license: "Marvel",
+        details: "Add a bold new look to your Marvel collection with this reimagined take on a fan favorite! Inspired by his original Funko Pop! debut, Pop! Black Panther returns with a fresh design, ready to stand tall among your heroes. Bring home the king of Wakanda and let the legend live on in your display. This vinyl bobblehead stands approximately 3.8 inches (9.7 cm) tall.",
+        price: 12,
 
     },
 ]
--- src/data/graphicnovels.js
+++ src/data/graphicnovels.js
@@ -1,14 +1,14 @@
 const graphicNovels = [
       {
-    _id: "",
-    title: "",
-    author: "",
-    artist: "",
-    publisher: "",
-    published: "",
-    about: "",
+    _id: "001",
+    title: "MILESTONE COMPENDIUM VOL. 1 TP",
+    author: "Dwayne McDuffie, Bob Smith",
+    artist: "Denys Cowan, M.D. Bright",
+    publisher: "Milestone Comics",
+    published: "2022",
+    about: "The Milestone Compendium is a must-have for fans of Milestone Comics and ‘90s comic book history. This epic collection brings together some of the most iconic and influential stories of the era, featuring writing from Dwayne McDuffie and Bob Smith, with dynamic artwork by Denys Cowan and M.D. Bright. Packed with fan-favorite titles, the compendium includes Blood Syndicate #1–12, Hardware #1–12, Icon #1–10, Static #1–8, Xombi #0–11, and Shadow Cabinet #0. Whether you’re a longtime fan or discovering these groundbreaking characters for the first time, this collection delivers a powerful dose of superhero storytelling that helped shape the modern comics landscape.",
     imageUrl: "",
-    price: 1,
+    price: 59.99,
   },
 ]
 
