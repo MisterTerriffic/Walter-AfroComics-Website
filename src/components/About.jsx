ad8edbba205ec111ca45f105af0e00cb17d4c9fe
Walter Fisher <o.b.i.e@Mac.lan>
2025-11-25 18:24:06 -0600

    updated about statesment.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,7 @@ function About() {
          Throughout history, people of color have had to create their own spaces because access to mainstream platforms was limited for them. Here we 
          showcase, inform, expose, and sell content made by Black creators who have had to forge their own terms. We auction off rare items and sell 
          material from comic books, toys, action figures, Funko Pop collectibles, and more. People in the minority need places where they can celebrate 
-         themselves and see themselves. We provide a space for independent creators and beyond.
+         themselves and see themselves. We provide a space for independent creators and beyond. The site is currently under construction and nothing is on sale yet. All will come in due time.
         </p>
       </div>
     </main>
